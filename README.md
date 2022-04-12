e97a001e3202352511fd540dc435498c82ca3507
TonyStark3ooo <55956724+TonyStark3ooo@users.noreply.github.com>
2022-04-12 12:59:56 +0530

    established mail sending channel(sendgrid) + add new collums for varification and unsubcribe hash code

--- README.md
+++ README.md
@@ -1,7 +1,11 @@
-# php-starter
+# RT Camp Assignment
 
-If you are reading this in your assignment repo, please read [assignment submission guidelines](https://learn.rtcamp.com/campus/php-assignments/guidelines/) before proceeding.
+## Problem Statement
 
-Then make sure to replace content of the file with information relevant to your assignment. 
+    **Please create a simple PHP application that accepts a visitor’s email address and emails them random XKCD comics every five minutes.**
+    1. Your app should include email verification to avoid people using others’ email addresses.
+    2. XKCD image should go as an email attachment as well as inline image content.
+    3. You can visit https://c.xkcd.com/random/comic/ programmatically to return a random comic URL and then use JSON API for details https://xkcd.com/json.html
+    4. Please make sure your emails contain an unsubscribe link so a user can stop getting emails. 
 
-Writing your own README.md file is required anyway as per [assignment submission guidelines](https://learn.rtcamp.com/campus/php-assignments/guidelines/).
+## How to use this solution
--- partials/_helpers.php
+++ partials/_helpers.php
@@ -1,6 +1,9 @@
 <?php
 
     include '_dbConnect.php';
+    include './private/__API_KEYS.php';
+  
+    
     
     function randomHex16(){
         return bin2hex(random_bytes(16)); 
@@ -47,4 +50,44 @@
         return $var == 0;
     }
 
+    function sendMails($emails,$body){
+        global $SendGrid_API;
+        $name = "JanakPatel";
+        $subject = "random XKCD comic";
+        $headers = array(
+            'Authorization: Bearer '.$SendGrid_API,
+            'Content-Type: application/json'
+        );
+
+        $data = array(
+            "personalizations" => array(
+                array(
+                    "to" =>$emails //array of emails       
+                )
+            ),
+            "from" => array(
+                "email" => "2018ecjan068@gmail.com",
+                "name" => $name
+            ),
+            "subject" => $subject,
+            "content" => array(
+                array(
+                    "type" => "text/html",
+                    "value" => $body
+                )
+            )
+        );
+
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, "https://api.sendgrid.com/v3/mail/send");
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        $response = curl_exec($ch);
+        curl_close($ch);
+
+        return $response;
+    }
 ?>
